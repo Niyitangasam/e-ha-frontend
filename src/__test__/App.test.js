7118b46e6686fa21d8dafb40ad9c9b2c0e5fa5a6
Niyitangasam <niyitangasam@gmail.com>
2020-01-24 13:50:08 +0200

    fix import

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../components/app';
+import App from '../components/App';
 describe('app component', () => {
   it('should render without error', () => {
     const wrapper = shallow(<App/>);
